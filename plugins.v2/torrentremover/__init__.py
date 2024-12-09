ccb3d18c14d0862e5ed4989f2cbcdbe1e475ff95
whc95800 <82248817+whc95800@users.noreply.github.com>
2024-12-10 08:51:11 +0900

    更新版本号

--- plugins.v2/torrentremover/__init__.py
+++ plugins.v2/torrentremover/__init__.py
@@ -26,7 +26,7 @@ class TorrentRemover(_PluginBase):
     # 插件图标
     plugin_icon = "delete.jpg"
     # 插件版本
-    plugin_version = "2.1.2"
+    plugin_version = "2.1.3"
     # 插件作者
     plugin_author = "jxxghp"
     # 作者主页
